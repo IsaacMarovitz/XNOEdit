aa6f1e797e28ebfe7cd32b0600e04c2f671f6cb6
Isaac Marovitz <isaacryu@icloud.com>
2025-11-18 01:29:00 -0500

    Better default sun color
    Signed-off-by: Isaac Marovitz <isaacryu@icloud.com>

--- XNOEdit/RenderSettings.cs
+++ XNOEdit/RenderSettings.cs
@@ -9,7 +9,7 @@ namespace XNOEdit
         public bool VertexColors = true;
         public bool BackfaceCulling = false;
         public Vector3 SunDirection = Vector3.Normalize(new Vector3(0.5f, 0.5f, 0.5f));
-        public Vector3 SunColor = Vector3.Normalize(new Vector3(1.0f, 0.95f, 0.8f));
+        public Vector3 SunColor = new(0.98f, 0.94f, 0.91f);
 
         public RenderSettings() { }
     }
